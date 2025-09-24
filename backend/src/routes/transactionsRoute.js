36a70867afa4f8784093cefe408442cfe42dc2b4
emman355 <earevalo355@gmail.com>
2025-09-25 00:47:10 +0800

    initial setup for backend and frontend

---
+++ backend/src/routes/transactionsRoute.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { createTransaction, deleteTransaction, getTransactionsByUserId, getTransactionSummary } from '../controllers/transactionsController.js'
+
+const router = express.Router()
+
+router.get("/:userId", getTransactionsByUserId)
+router.post("/", createTransaction)
+router.delete("/:id", deleteTransaction)
+router.get("/summary/:userId", getTransactionSummary)
+
+export default router
