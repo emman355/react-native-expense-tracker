0548307442fef78dc8c109fa1bfd3fa64cc26b94
emman355 <earevalo355@gmail.com>
2025-09-25 09:31:08 +0800

    feat: setup clerk for authentication

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,23 @@
-import { Stack } from "expo-router";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { Slot } from 'expo-router'
+import SafeScreen from '@/components/SafeScreen'
+import Constants from "expo-constants";
+
+const clerkPublishableKey =
+  Constants?.expoConfig?.extra?.clerkPublishableKey ||
+  process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
 
 export default function RootLayout() {
-  return <Stack />;
+  if (!clerkPublishableKey) {
+    throw new Error("Missing Clerk Publishable Key!");
+  }
+
+  return (
+    <ClerkProvider tokenCache={tokenCache} publishableKey={clerkPublishableKey}>
+      <SafeScreen>
+        <Slot />
+      </SafeScreen>
+    </ClerkProvider>
+  )
 }
